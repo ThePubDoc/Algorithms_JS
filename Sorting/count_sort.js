dbb61ebe2e8523b57cd79eb40b1f7f287f6224fd
Aayush <aayush9152@gmail.com>
2019-06-16 22:46:28 +0530

    added count sort

---
+++ Sorting/count_sort.js
@@ -0,0 +1,28 @@
+function cs(arr){
+    var count=[];
+    
+    var min=Math.min(...arr);
+    var max=Math.max(...arr);
+
+    for(i=min;i<=max;i++){
+        count[i]=0;
+    }
+
+    for(i=0;i<arr.length;i++){
+        count[arr[i]]++;
+    }
+    
+    var sortedArray=[];
+
+    for(i=0;i<count.length;i++){
+        if(count[i]!==0){
+            for(j=0;j<count[i];j++){
+                sortedArray.push(i);
+            }
+        }
+    }
+    return sortedArray;
+}
+
+
+console.log(cs([1,3,4,3,2,1,2,2,3,4,4,5,3,2,1,2,33,23]));
